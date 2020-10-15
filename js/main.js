4f2d075470e581c9abd5d6270d52859326c6b9ae
leo-leb <swan.leon.id@gmail.com>
2020-10-15 17:01:04 +0300

    4.1-added-filling-adress-form-option-v1

--- js/main.js
+++ js/main.js
@@ -3,6 +3,9 @@
 const OFFSET_X = 200;
 const OFFSET_Y = 400;
 const LIB_SIZE = 7;
+const PIN_X = 65;
+const PIN_Y_START = 65;
+const PIN_Y_ACTIVE = 82;
 
 let libType = [`palace`, `flat`, `house`, `bungalow`];
 let libCheck = [`12:00`, `13:00`, `14:00`];
@@ -15,10 +18,43 @@ const newItemPin = pinTemplate.querySelector(`.map__pin`);
 const mainPin = mapPins.querySelector(`.map__pin--main`);
 const formParent = document.querySelector(`.ad-form`);
 const formChilds = formParent.querySelectorAll(`fieldset`);
+const formAddress = document.querySelector(`#address`);
 
 const map = document.querySelector(`.map`);
 const mapFilterParent = map.querySelector(`.map__filters`);
 const mapFilterChilds = mapFilterParent.children;
+
+/**
+ * Возвращает массив расчитанных координат исследуемого объекта для стартовой страницы.
+ * @param {number} width - Ширина объекта.
+ * @param {number} height - Высота объекта.
+ * @return {array} - Массив координат объекта.
+ */
+const calcPositionStart = function (width, height) {
+  const array = [Math.round(map.offsetWidth / 2 - width / 2).toString(), Math.round(map.offsetHeight / 2 - height / 2).toString()];
+  return array;
+};
+
+/**
+ * Возвращает массив расчитанных координат исследуемого объекта для активной страницы.
+ * @param {number} width - Ширина объекта.
+ * @param {number} height - Высота объекта.
+ * @return {array} - Массив координат объекта.
+ */
+const calcPositionActive = function (width, height) {
+  const array = [Math.round(map.offsetWidth / 2 - width / 2).toString(), Math.round(map.offsetHeight / 2 - height).toString()];
+  return array;
+};
+
+/**
+ * Присваивает актуальные координаты исходному элементу разметки.
+ * @param {*} object - Исходный элемент разметки.
+ * @param {array} objectPos - Массив актуальных координат.
+ */
+const setPosition = function (object, objectPos) {
+  object.style.left = objectPos[0] + `px`;
+  object.style.top = objectPos[1] + `px`;
+};
 
 /**
  * Присваивает неактивное состояние всем дочерним элементам исходного массива.
@@ -46,6 +82,9 @@ const setActiveCondition = function (array) {
 const setPassivePage = function () {
   setPassiveCondition(mapFilterChilds);
   setPassiveCondition(formChilds);
+  const mainPinPos = calcPositionStart(PIN_X, PIN_Y_START);
+  setPosition(mainPin, mainPinPos);
+  formAddress.value = mainPinPos[0] + `, ` + mainPinPos[1];
 };
 
 /**
@@ -56,6 +95,9 @@ const setActivePage = function () {
   formParent.classList.remove(`ad-form--disabled`);
   setActiveCondition(mapFilterChilds);
   setActiveCondition(formChilds);
+  const mainPinPos = calcPositionActive(PIN_X, PIN_Y_ACTIVE);
+  setPosition(mainPin, mainPinPos);
+  formAddress.value = mainPinPos[0] + `, ` + mainPinPos[1];
 };
 
 setPassivePage();
